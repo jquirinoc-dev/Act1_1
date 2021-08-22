eeebd20fd118db784b4a5181d634819583caaea1
Juan Pablo <jppd0657@gmail.com>
2021-08-21 19:42:36 -0600

    Solo faltan los grados de complejidad

--- mainAct1_1.cpp
+++ mainAct1_1.cpp
@@ -50,12 +50,12 @@ int fibonacciIterativo(int n){
 
             if (i == 1 || i == 2) {
 
-                std::cout << "F(" << i << "): " << 1 << std::endl;       
+                std::cout << "F(" << i << ") = " << 1 << " ";       
 
             } else {
 
                 resultado = buffer + buffer_;
-                std::cout << "F(" << i << "): " << resultado << std::endl; 
+                std::cout << "F(" << i << ") = " << resultado << " "; 
                 buffer_ = buffer;
                 buffer = resultado; 
 
@@ -63,7 +63,7 @@ int fibonacciIterativo(int n){
         }
     }
 
-    return 1;
+    return resultado;
 
 }
 
@@ -81,20 +81,84 @@ int fibonacciRecursivo(int n){
 
 int main(){
 
-    std::cout << "Suma por Iteracion: " << sumaIterativa(6) << std::endl;
-    std::cout << "Suma por Recursion: " << sumaRecursiva(6) << std::endl;
-    std::cout << "Suma directa: " << sumaDirecta(6) << std::endl;
+    std::cout << "| TEST CASE n = 20 |" << std::endl;
+    std::cout << "Suma por Iteracion: " << sumaIterativa(20) << std::endl;
+    std::cout << "Suma por Recursion: " << sumaRecursiva(20) << std::endl;
+    std::cout << "Suma directa: " << sumaDirecta(20) << std::endl;
 
     std::cout << "\n";
 
+    std::cout << "| TEST CASE n = 50 |" << std::endl;
+    std::cout << "Suma por Iteracion: " << sumaIterativa(50) << std::endl;
+    std::cout << "Suma por Recursion: " << sumaRecursiva(50) << std::endl;
+    std::cout << "Suma directa: " << sumaDirecta(50) << std::endl;
+
+    std::cout << "\n";
+
+    std::cout << "| TEST CASE n = 100 |" << std::endl;
+    std::cout << "Suma por Iteracion: " << sumaIterativa(100) << std::endl;
+    std::cout << "Suma por Recursion: " << sumaRecursiva(100) << std::endl;
+    std::cout << "Suma directa: " << sumaDirecta(100) << std::endl;
+
+    std::cout << "\n";
+
+    std::cout << "| TEST CASE n = 1000 |" << std::endl;
+    std::cout << "Suma por Iteracion: " << sumaIterativa(1000) << std::endl;
+    std::cout << "Suma por Recursion: " << sumaRecursiva(1000) << std::endl;
+    std::cout << "Suma directa: " << sumaDirecta(1000) << std::endl;    
+
+    std::cout << "\n";
+
+
     std::cout << "Fibonacci por Iteración: " << std::endl;
-    fibonacciIterativo(8);
+
+    std::cout << "| TEST CASE n = 5 | ";
+    fibonacciIterativo(5);
+
+    std::cout << "\n";
+
+    std::cout << "| TEST CASE n = 10 | ";
+    fibonacciIterativo(10);
+
+    std::cout << "\n";
+
+    std::cout << "| TEST CASE n = 15 | ";
+    fibonacciIterativo(15);
+
+    std::cout << "\n";
+
+    std::cout << "| TEST CASE n = 30 | ";
+    fibonacciIterativo(30);
+
+    std::cout << "\n";
+    std::cout << "\n";
 
     std::cout << "Fibonacci por Recursion: " << std::endl;
-    for (int i = 1; i < 9; i++) {
-        std::cout << "F(" << i << "): " << fibonacciRecursivo(i) << std::endl;
+
+    std::cout << "| TEST CASE n = 5 | ";
+    for (int i = 1; i < (5) + 1; i++) {
+        std::cout << "F(" << i << ") = " << fibonacciRecursivo(i) << " ";
     }
+    std::cout << "\n";
+
+    std::cout << "| TEST CASE n = 10 | ";
+    for (int i = 1; i < (10) + 1; i++) {
+        std::cout << "F(" << i << ") = " << fibonacciRecursivo(i) << " ";
+    }
+    std::cout << "\n";
+
+    std::cout << "| TEST CASE n = 15 | ";
+    for (int i = 1; i < (15) + 1; i++) {
+        std::cout << "F(" << i << ") = " << fibonacciRecursivo(i) << " ";
+    }
+    std::cout << "\n";
+
+    std::cout << "| TEST CASE n = 30 | ";
+    for (int i = 1; i < (30) + 1; i++) {
+        std::cout << "F(" << i << ") = " << fibonacciRecursivo(i) << " ";
+    }
+    std::cout << "\n";
 
     return 0;
-    
+
 }
